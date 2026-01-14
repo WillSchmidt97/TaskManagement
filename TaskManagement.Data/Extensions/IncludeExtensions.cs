b2fadab2e5cb6009e99749e8b2814960fe55e00d
WillSchmidt97 <willschmidt1997@gmail.com>
2026-01-14 17:23:04 -0300

    added helpers

---
+++ TaskManagement.Data/Extensions/IncludeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Values.Objects;
+
+namespace TaskManagement.Data.Extensions
+{
+    public static class IncludeExtensions
+    {
+        public static IQueryable<T> AddInclude<T>(this IQueryable<T> query, Include? includes) where T : class
+        {
+            if (includes is null || !includes.Includes.Any())
+                return query;
+
+            foreach (var include in includes.Includes)
+                query = query.Include(include);
+
+            return query;
+        }
+    }r
+}
---
+++ TaskManagement.Data/Extensions/PagedResultExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain.Values.Objects;
+
+namespace TaskManagement.Data.Extensions
+{
+    public static class PagedResultExtensions
+    {
+        public static PagedResult<T> ToPagedResult<T>(this IQueryable<T> query, int page, int size)
+        where T : class
+        {
+            var totalCount = query.Count();
+
+            var items = query
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToList();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                Pagination = new Pagination(page, size, totalCount)
+            };
+        }
+
+        public static async Task<PagedResult<T>> ToPagedResultAsync<T>(this IQueryable<T> query, int page, int size)
+        where T : class
+        {
+            var totalCount = await query.CountAsync();
+
+            var items = await query
+                .Skip((page - 1) * size)
+                .Take(size)
+                .ToListAsync();
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                Pagination = new Pagination(page, size, totalCount)
+            };
+        }
+    }
+}
