5c5167fd4e1352ff3ff9d9e0ac46e5c6c2900be5
WillSchmidt97 <willschmidt1997@gmail.com>
2026-01-16 07:48:06 -0300

    setted app db context

---
+++ TaskManagement.Data/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Data.Mappings;
+using TaskManagement.Domain.Entities;
+
+namespace TaskManagement.Data.Context
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<TaskItem> TaskItems => Set<TaskItem>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TaskMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
